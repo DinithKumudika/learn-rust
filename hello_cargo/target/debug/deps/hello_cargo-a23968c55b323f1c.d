b8b3b1816f6862c143c99edb66921137b4a36df0
DinithKumudika <dinithwalpitagama@gmail.com>
2023-02-24 21:18:48 +0530

    Add files via upload

---
+++ hello_cargo/target/debug/deps/hello_cargo-a23968c55b323f1c.d
@@ -0,0 +1,5 @@
+d:\dev\rust\hello_cargo\target\debug\deps\hello_cargo-a23968c55b323f1c.rmeta: src\main.rs
+
+d:\dev\rust\hello_cargo\target\debug\deps\hello_cargo-a23968c55b323f1c.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+D:\dev\rust\hello_cargo\target\debug\hello_cargo.exe: D:\dev\rust\hello_cargo\src\main.rs
